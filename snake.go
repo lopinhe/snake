a61b266a1cf9517f2bdd92fb5ea3d2021af47ed1
lopinhe <lopinhe@proton.me>
2023-09-16 18:28:12 +0200

    1st

---
+++ snake.go
@@ -0,0 +1,39 @@
+package snake
+
+import (
+	"fmt"
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+const (
+	ScreenWidth  = 600
+	ScreenHeight = 600
+	boardRows    = 20
+	boardCols    = 20
+)
+
+var (
+	backgroundColor = color.RGBA{50, 100, 50, 50}
+)
+
+type Game struct {}
+
+func NewGame() *Game {
+	return &Game{}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return ScreenWidth, ScreenHeight
+}
+
+func (g *Game) Update() error {
+	// todo
+    return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(backgroundColor)
+}
